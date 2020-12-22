407838dc168904262b4801e9835dacd813b39f9f
imeuro <mauro.fioravanzi@gmail.com>
2020-12-22 10:37:21 +0100

    cachebuster

--- wp-content/plugins/catalogo-ragionato/index.php
+++ wp-content/plugins/catalogo-ragionato/index.php
@@ -23,8 +23,8 @@ function catalogo_scriptsnstyles() {
 
 	// echo 'TEST $cacheBusterJS: '.$cacheBusterJS.' | '.plugin_dir_url( __FILE__ ) . 'catalogo-ragionato.js?'.$cacheBusterJS;
 
-  wp_enqueue_style( 'cataloogo', plugin_dir_url( __FILE__ ) .'catalogo-ragionato.css?jj', array(), $cacheBusterCSS, 'all');
-  wp_enqueue_script( 'catalogo', plugin_dir_url( __FILE__ ) . 'catalogo-ragionato.js?jj', array('swiper'), $cacheBusterJS, true );
+  wp_enqueue_style( 'cataloogo', plugin_dir_url( __FILE__ ) .'catalogo-ragionato.css?j', array(), $cacheBusterCSS, 'all');
+  wp_enqueue_script( 'catalogo', plugin_dir_url( __FILE__ ) . 'catalogo-ragionato.js?j', array('swiper'), $cacheBusterJS, true );
 }
 add_action( 'wp_enqueue_scripts', 'catalogo_scriptsnstyles' );
 
